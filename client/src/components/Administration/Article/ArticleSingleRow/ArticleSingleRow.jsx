fd0d1e2239b7a1533a95cf5794137265d0c5a364
christinanikolova <christinanikolova@github.com>
2021-10-03 09:27:40 +0300

    write tests for administration

--- client/src/components/Administration/Article/ArticleSingleRow/ArticleSingleRow.jsx
+++ client/src/components/Administration/Article/ArticleSingleRow/ArticleSingleRow.jsx
@@ -24,8 +24,8 @@ function ArticleSingleRow({ id, title, picture, categoryId, categoryName, likesC
             <td className="color-link"><Link to={`/articles/current-article/${id}`}>{title}</Link></td>
             <td><img className="img-article-admin" src={picture} alt="article-pic"></img></td>
             <td className="color-link"><Link to={`/articles/by-category/${categoryId}`}>{categoryName}</Link></td>
-            <td>{likesCount}</td>
-            <td>{commentsCount}</td>
+            <td className="aricles-likes-count">{likesCount}</td>
+            <td className="aricles-comments-count">{commentsCount}</td>
             <td><Link to={`/admin/articles/update/${id}`}><button className="btn btn-warning">Update</button></Link></td>
             <td><button className="btn btn-danger" onClick={remove} >Delete</button></td >
         </tr >
--- client/src/components/Administration/Category/CategorySingleRow/CategorySingleRow.jsx
+++ client/src/components/Administration/Category/CategorySingleRow/CategorySingleRow.jsx
@@ -22,7 +22,7 @@ function CategorySingleRow({ id, name, articlesCount, picture, clickHandler }) {
     return (
         <tr className="text-center">
             <td className="color-link"><Link to={`/articles/by-category/${id}`}>{name}</Link></td>
-            <td>{articlesCount}</td>
+            <td className="articles-count">{articlesCount}</td>
             <td><img className="img-category-admin" src={picture} alt="category-pic"></img></td>
             <td><Link to={`/admin/categories/update/${id}`}><button className="btn btn-warning">Update</button></Link></td>
             <td><button className="btn btn-danger" onClick={remove} >Delete</button></td >
