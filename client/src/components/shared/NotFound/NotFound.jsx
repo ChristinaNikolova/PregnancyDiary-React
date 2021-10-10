b760aa08d56204c327ad44bfb87625516f8d634e
christinanikolova <christinanikolova@github.com>
2021-10-10 07:52:27 +0300

    add tests to NotFound component

--- client/src/components/shared/NotFound/NotFound.jsx
+++ client/src/components/shared/NotFound/NotFound.jsx
@@ -7,7 +7,7 @@ function NotFound() {
         <div className="not-found-wrapper text-center">
             <img src="./koala-and-mama-malta-babywearing-404-page-not-found.png"></img>
             <div className="msg custom-font">Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in the first place?
-            <p>Let's go <Link to="/" className="home-link">home</Link> and try from there.</p>
+               <p>Let's go <Link to="/" className="home-link">home</Link> and try from there.</p>
             </div>
         </div>
     );
---
+++ client/src/components/shared/NotFound/NotFound.test.js
@@ -0,0 +1,21 @@
+import { render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import NotFound from "./NotFound";
+
+describe("NotFound Component", () => {
+    it("should render the component", () => {
+        render(
+            <BrowserRouter>
+                <NotFound />
+            </BrowserRouter>
+        );
+
+        expect(document.getElementsByClassName('not-found-wrapper')[0]).toBeTruthy();
+        expect(document.getElementsByTagName('img')[0].getAttribute('src')).toBe('./koala-and-mama-malta-babywearing-404-page-not-found.png');
+        expect(document.getElementsByClassName('msg')[0].textContent)
+        .toBe('Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in the first place?Let\'s go home and try from there.');
+        expect(document.getElementsByTagName('a').length).toBe(1);
+        expect(document.getElementsByTagName('a')[0].getAttribute('href')).toBe('/');
+        expect(document.getElementsByTagName('a')[0].textContent).toBe('home');
+    });
+});
